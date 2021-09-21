221b5bc8e5a5abbeec17d7e5f8fba7a84ccd8c64
ravi-shyam <ravis30698@gmail.com>
2021-09-21 15:32:30 +0530

    Introduced layout component to blog post

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,9 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import Layout from '../../components/layout'
 export default function FirstPost() {
     return (
-        <>
+        <Layout>
             <Head>
                 <title>
                     My first post
@@ -14,6 +15,6 @@ export default function FirstPost() {
                     Back Home?
                 </Link>
             </h2>
-        </>
+        </Layout>
     )
 }
