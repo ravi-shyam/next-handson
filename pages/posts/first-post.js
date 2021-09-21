899c14a6ee83891f72936f7651fd90d43c7b7c81
ravi-shyam <ravis30698@gmail.com>
2021-09-21 15:16:02 +0530

    Changed title of first post page

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,7 +1,13 @@
-import Link from 'next/link' 
+import Link from 'next/link'
+import Head from 'next/head'
 export default function FirstPost() {
     return (
         <>
+            <Head>
+                <title>
+                    My first post
+                </title>
+            </Head>
             <h1>First Post</h1>
             <h2>
                 <Link href='/'>
